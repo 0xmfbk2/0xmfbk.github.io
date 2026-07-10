84658cc684ac86105cce91c24b62687d0537cd7e
0xmfbk2 <2022506166@ses.yu.edu.jo>
2026-07-10 08:02:44 +0300

    Some Updates

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   // 1. بما أن GitHub Pages تستضيف ملفات ثابتة، نحتاج لتغيير الـ base
   // استبدل 'repo-name' باسم المستودع الخاص بك على GitHub
-  base: "/0xmfbk.github.io/", 
+  base: "/", 
 
   // 2. إزالة إعدادات الـ nitro لأنها خاصة بالسيرفرات وليست للمواقع الثابتة
   // (GitHub Pages لا تحتاج nitro)
