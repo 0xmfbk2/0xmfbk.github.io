7fc0c4f7dbd7351d7ac50aaa33005cf936d3b97d
0xmfbk2 <2022506166@ses.yu.edu.jo>
2026-07-10 09:13:33 +0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,5 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  nitro: {
-    preset: "netlify", // هذا أهم تغيير
-  },
-  tanstackStart: {
-    server: { entry: "server" },
-  },
+  base: "/", 
 });
