156c7a849c70c25e6f10fbaf38585d3ecf216afd
0xmfbk2 <2022506166@ses.yu.edu.jo>
2026-07-10 09:00:20 +0300

    Some Updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  // Vercel يدعم الـ SSR، لذا نترك الإعدادات الافتراضية للـ Nitro
   nitro: {
-    preset: "vercel", // نخبره أننا نستضيف على Vercel
+    preset: "netlify", // هذا أهم تغيير
   },
   tanstackStart: {
     server: { entry: "server" },
